d660a3a824b75580068b83cef07b408079bd95a0
Jhurl123 <34147439+Jhurl123@users.noreply.github.com>
2018-03-19 21:02:19 -0400

    Add files via upload

---
+++ Ch9_Ex01/src/Ch9_Ex01.cpp
@@ -0,0 +1,92 @@
+//============================================================================
+// Name        : Ch9_Ex01.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "arrayListType.h"
+using namespace std;
+
+
+//lower level function prototype
+void printListInfo(arrayListType<int>& passedList);
+
+// object instantiation
+				arrayListType<int> myIntList;
+
+int main()
+{
+
+
+				//declare the variables
+				int userInput = 0;
+				int numLocation = 0;
+
+				//int first = myIntList.list[0];
+
+
+			cout << "WELCOME TO THE ARRAY POSITION FINDER";
+			cout << endl << endl;
+
+			cout << "Enter integer values (-999 to Quit): " << endl;
+			cin >> userInput;
+			while ( userInput != -999)
+			{
+
+				myIntList.insert( userInput);
+				cin >> userInput;
+
+			}
+
+				printListInfo(myIntList);
+				cout << endl << endl;
+
+				cout << "What number in the list do you want to search for? ";
+				cin >> userInput;
+				numLocation = myIntList.recSeqSearchImp(userInput);
+				cout << numLocation << endl;
+				if (numLocation != -1)
+				{
+					cout << userInput << " was found in position " << numLocation  << " of the list.";
+				}
+				else
+				{
+					cout << userInput + " was not found in the list....";
+				}
+				cout << endl << endl ;
+				cout << "End of Program.";
+return 0;
+
+
+}
+void printListInfo(arrayListType<int>& passedList)
+{
+	int size = passedList.listSize();
+	cout << endl;
+	cout << "The list you entered --- > " ;
+	passedList.print();
+	cout << endl << "List Size is: " << size;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
